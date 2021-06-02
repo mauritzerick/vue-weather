a8f2b804c7b11c17153c44cebc97faed0ce371cb
Mauritz Erick <mauritz.erick@gmail.com>
2021-06-03 00:52:10 +1000

    broken code test

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<!-- Broken code on line 16 -->
+<!-- Broken code on line 16 test test -->
 
 <template>
   <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
