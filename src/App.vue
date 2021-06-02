2ac5c84cf500b470191dcc4ecee46a8ea52c8a3f
Mauritz Erick <mauritzerick@192-168-1-101.tpgi.com.au>
2021-06-02 23:04:12 +1000

    search box

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
-
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search...."/>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
+  name: 'app',
+  data () {
+    return {
+      api_key: '69e3373095f6144c154a3c430fa56813'
+
+    }
+  }
  
 }
 </script>
